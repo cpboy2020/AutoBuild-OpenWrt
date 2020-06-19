84aa83d69f197447c352ba0c88ce2fdab33a76f3
cpboy2020 <66666045+cpboy2020@users.noreply.github.com>
2020-06-19 19:07:21 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,7 +10,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 
 # Add Compile-ID(cid)
 cid=$(date "+%Y-%m-%d")
-sed -i "s/R20.5.20/R20.5.20[${cid}]/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/R20.5.20/R20.5.20[${cid}]/g" openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 自定义定制选项
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings #取消系统默认密码
