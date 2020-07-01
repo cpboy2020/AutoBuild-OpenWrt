28f1c16067d400c03db26e261a38d03a9dc514f2
cpboy2020 <66666045+cpboy2020@users.noreply.github.com>
2020-07-02 06:52:26 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -54,29 +54,29 @@ sed -i 's/services/control/g'  openwrt/package/lean/luci-app-accesscontrol/luasr
 
 sed -i 's/\"system\"/\"control\"/g' openwrt/package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
 
-sed -i 's/\"services\"/\"dns\"/g' openwrt/package/feeds/kenzo/luci-app-adguardhome/luasrc/controller/AdGuardHome.lua
-sed -i 's/services/dns/g' openwrt/package/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/AdGuardHome_check.htm
-sed -i 's/services/dns/g' openwrt/package/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/AdGuardHome_status.htm
-sed -i 's/services/dns/g' openwrt/package/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/log.htm
-sed -i 's/services/dns/g' openwrt/package/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/yamleditor.htm
+sed -i 's/\"services\"/\"dns\"/g' openwrt/feeds/kenzo/luci-app-adguardhome/luasrc/controller/AdGuardHome.lua
+sed -i 's/services/dns/g' openwrt/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/AdGuardHome_check.htm
+sed -i 's/services/dns/g' openwrt/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/AdGuardHome_status.htm
+sed -i 's/services/dns/g' openwrt/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/log.htm
+sed -i 's/services/dns/g' openwrt/feeds/kenzo/luci-app-adguardhome/luasrc/view/AdGuardHome/yamleditor.htm
 
-sed -i 's/\"services\"/\"vpn\"/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/controller/clash.lua
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/conf.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/core_check.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/game_rule.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/geoip.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/list.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/start_stop.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/status.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/status_log.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-clash/luasrc/view/clash/update.htm
+sed -i 's/\"services\"/\"vpn\"/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/controller/clash.lua
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/conf.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/core_check.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/game_rule.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/geoip.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/list.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/start_stop.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/status.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/status_log.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-clash/luasrc/view/clash/update.htm
 
-sed -i 's/\"services\"/\"vpn\"/g' openwrt/package/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/download_rule.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/server_list.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/state.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/status.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/update.htm
+sed -i 's/\"services\"/\"vpn\"/g' openwrt/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/download_rule.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/server_list.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/state.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/status.htm
+sed -i 's/services/vpn/g' openwrt/feeds/kenzo/luci-app-openclash/files/usr/lib/lua/luci/view/openclash/update.htm
 
 sed -i 's/\"services\"/\"dns\"/g' openwrt/feeds/kenzo/luci-app-aliddns/luasrc/controller/aliddns.lua
 
@@ -88,11 +88,11 @@ sed -i 's/\"services\"/\"dns\"/g' openwrt/package/lean/luci-app-dnsforwarder/lua
 sed -i 's/\"services\"/\"nas\"/g' openwrt/package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
 sed -i 's/services/nas/g' openwrt/package/lean/luci-app-familycloud/luasrc/view/familycloud/familycloud_status.htm
 
-sed -i 's/\"services\"/\"vpn\"/g' openwrt/package/feeds/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
-sed -i 's/services/vpn/g' openwrt/package/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/check.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/checkport.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/refresh.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/status.htm
-sed -i 's/services/vpn/g' openwrt/package/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/subscribe.htm
+sed -i 's/\"services\"/\"vpn\"/g' openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+sed -i 's/services/vpn/g' openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/check.htm
+sed -i 's/services/vpn/g' openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/checkport.htm
+sed -i 's/services/vpn/g' openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/refresh.htm
+sed -i 's/services/vpn/g' openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/status.htm
+sed -i 's/services/vpn/g' openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/subscribe.htm
 
 #sed -i 's/\""network"\"/\"nas\"/g' openwrt/package/lean/*/luasrc/controller/eqos.lua
