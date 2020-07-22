f21c8edcc5d9c2eb387689b86073c60defba756d
cpboy2020 <66666045+cpboy2020@users.noreply.github.com>
2020-07-22 12:54:06 +0800

    Update yunleandiy.sh

--- yunleandiy.sh
+++ yunleandiy.sh
@@ -12,9 +12,17 @@
 #cid=$(date "+%Y-%m-%d")
 #sed -i "s/R20.6.18/R20.6.20[${cid}] Compiled by cpboy/g" openwrt/package/lean/default-settings/files/zzz-default-settings
 
+date=`date +%m.%d.%Y`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D Compiled by cpboy'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+
 #3.添加自定义源代码
+
 git clone https://github.com/Lienol/openwrt-package package/lienol-package
 git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+
+git clone https://github.com/lisaac/luci-app-dockerman package/cpboy/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker package/cpboy/packages/luci-lib-docker #git luci-app-dockerman 依赖
 
 #git clone https://github.com/liuwenwv/luci-app-vssr-plus package/copy/luci-app-vssr-plus
 #git clone https://github.com/Leo-Jo-My/diy package/cpboy/packages  #依赖包
@@ -55,10 +63,15 @@ rm -rf package/cpboy/packages/openwrt-v2ray-plugin
 
 rm -rf package/lean/luci-theme-argon
 
-git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon package/copy/luci-theme-atmaterial-ColorIcon
-git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark  package/cpboy/luci-theme-Butterfly-dark
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/copy/luci-theme-infinityfreedom
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/cpboy/luci-theme-infinityfreedom
+git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon package/cpboy/luci-theme-atmaterial-ColorIcon
+git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/cpboy/luci-theme-Butterfly-dark
+git clone https://github.com/siropboy/luci-theme-btmod.git package/cpboy/luci-theme-btmod
+
 cd openwrt
+
+cp -rf ./dns.lua ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin
+
 # 更新feeds文件
 sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default #启用helloworld
 cat feeds.conf.default
@@ -72,31 +85,58 @@ cat feeds.conf.default
 #sed -i s/yyyy/xxxx/g `grep yyyy -rl --include="*.txt" ./`
 #作用：将当前目录(包括子目录)中所有txt文件中的yyyy字符串替换为xxxx字符串
 
-sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" package/kenzok8/luci-app-adguardhome/`
-sed -i 's/services/dns/g' `grep services -rl --include="*.htm" package/kenzok8/luci-app-adguardhome/`
+#sed -i 's/\"services\"/\"nas\"/g' `grep \"services\" -rl --include="*.lua" path`
+#sed -i 's/services/nas/g' `grep services -rl --include="*.htm" path`
 
-sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" package/kenzok8/luci-app-clash/`
-sed -i 's/services/vpn/g' `grep services -rl --include="*.htm" package/kenzok8/luci-app-clash/`
 
-sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" package/kenzok8/luci-app-openclash/`
-sed -i 's/services/vpn/g' `grep services -rl  --include="*.htm" package/kenzok8/luci-app-openclash/`
+sed -i 's/\"services\"/\"control\"/g' `grep \"services\" -rl --include="*.lua" ./package/lean/luci-app-accesscontrol/`
+sed -i 's/services/control/g' `grep services -rl --include="*.htm" ./package/lean/luci-app-accesscontrol/`
 
-sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" package/kenzok8/luci-app-aliddns/`
+sed -i 's/\"system\"/\"control\"/g' `grep \"services\" -rl --include="*.lua" ./package/lean/luci-app-autoreboot/`
 
-sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" package/kenzok8/luci-app-smartdns/`
-sed -i 's/services/dns/g' `grep services -rl --include="*.htm" package/kenzok8/luci-app-smartdns/`
+sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" ./package/lean/luci-app-dnsforwarder/`
+sed -i 's/services/dns/g' `grep services -rl --include="*.htm" ./package/lean/luci-app-dnsforwarder/`
 
-sed -i 's/\"services\"/\"control\"/g' package/lean/luci-app-accesscontrol/luasrc/controller/mia.lua
-sed -i 's/services/control/g' package/lean/luci-app-accesscontrol/luasrc/view/mia/mia_status.htm
 
-sed -i 's/\"system\"/\"control\"/g' package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
+sed -i 's/\"services\"/\"nas\"/g' `grep \"services\" -rl --include="*.lua" ./package/lean/luci-app-familycloud/`
+sed -i 's/services/nas/g' `grep services -rl --include="*.htm" ./package/lean/luci-app-familycloud/`
 
-sed -i 's/\"services\"/\"dns\"/g' package/lean/luci-app-dnsforwarder/luasrc/controller/dnsforwarder.lua
+sed -i 's/\"services\"/\"nas\"/g' `grep \"services\" -rl --include="*.lua" ./package/lean/luci-app-xlnetacc/`
+sed -i 's/services/nas/g' `grep services -rl --include="*.htm" ./package/lean/luci-app-xlnetacc/`
 
-sed -i 's/\"services\"/\"nas\"/g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
-sed -i 's/services/nas/g' package/lean/luci-app-familycloud/luasrc/view/familycloud/familycloud_status.htm
 
-sed -i 's/\"services\"/\"vpn\"/g' feeds/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
-sed -i 's/services/vpn/g' |xargs grep services -rl feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/*.htm
+sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" ./feeds/helloworld/luci-app-ssr-plus/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.lua" ./feeds/helloworld/luci-app-ssr-plus/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.htm" ./feeds/helloworld/luci-app-ssr-plus/`
 
-#sed -i 's/\""network"\"/\"nas\"/g' package/lean/*/luasrc/controller/eqos.lua
+sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" ./feeds/luci/applications/luci-app-ddns/`
+sed -i 's/services/dns/g' `grep services -rl --include="*.htm" ./feeds/luci/applications/luci-app-ddns/`
+
+sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" ./feeds/luci/applications/luci-app-dnscrypt-proxy/`
+sed -i 's/services/dns/g' `grep services -rl --include="*.htm" ./feeds/luci/applications/luci-app-dnscrypt-proxy/`
+
+sed -i 's/\"services\"/\"control\"/g' `grep \"services\" -rl --include="*.lua" ./feeds/luci/applications/luci-app-wol/`
+
+sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" ./feeds/luci/applications/luci-app-ocserv/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.htm" ./feeds/luci/applications/luci-app-ocserv/`
+
+sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" ./feeds/luci/applications/luci-app-openvpn/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.htm" ./feeds/luci/applications/luci-app-openvpn/`
+
+
+
+sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" ./package/kenzo/luci-app-adguardhome/`
+sed -i 's/services/dns/g' `grep services -rl --include="*.htm" ./package/kenzo/luci-app-adguardhome/`
+
+sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" ./package/kenzo/luci-app-aliddns/`
+sed -i 's/services/dns/g' `grep services -rl --include="*.htm" ./package/kenzo/luci-app-aliddns/`
+
+sed -i 's/\"services\"/\"dns\"/g' `grep \"services\" -rl --include="*.lua" ./package/kenzo/luci-app-smartdns/`
+sed -i 's/services/dns/g' `grep services -rl --include="*.htm" ./package/kenzo/luci-app-smartdns/`
+
+sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" ./package//kenzo/luci-app-clash/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.htm" ./package/kenzo/luci-app-clash/`
+
+sed -i 's/\"services\"/\"vpn\"/g' `grep \"services\" -rl --include="*.lua" ./package//kenzo/luci-app-openclash/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.lua" ./package//kenzo/luci-app-openclash/`
+sed -i 's/services/vpn/g' `grep services -rl --include="*.htm" ./package/kenzo/luci-app-openclash/`
